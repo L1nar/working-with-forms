4b9c072a3f13ffa6de0f992ecf5016790b3ef354
L1nar <122251664+L1nar@users.noreply.github.com>
2024-01-19 16:37:59 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 const form = document.getElementById('myForm');
 const btn = document.getElementById('sendBtn');
+
+// Все элементы управления формы, как бы глубоко они не находились в форме, доступны в коллекции form.elements
+btn.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log(form.elements.name.value);
+    console.log(form.elements.lastName.value);
+    if (form.elements.gender.value == 'm') {
+        console.log('мужчина');
+    } else {
+        console.log('женщина');
+    }
+    if (form.elements.auto.checked) {
+        console.log('Есть автомобиль');
+    } else {
+        console.log('Нет автомобиля');   
+    }
+})
